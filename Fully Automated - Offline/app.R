1f2b5df13135542bf54890f464bdff8c82c39c2d
mattsams89 <matt.l.sams@gmail.com>
2019-11-13 18:03:40 -0700

    Corrected jump 5SD - 30ms calculation to be responsive to sampling frequencies != 1000

--- Fully Automated - Offline/app.R
+++ Fully Automated - Offline/app.R
@@ -308,7 +308,7 @@ server <- function(input, output, session){
         else{
           jump_start_index <- detect_index(data[1:pre_movement_minimum_force_index, total_force],
                                            ~ .x >= inverse_threshold,
-                                           .dir = "backward") - 29
+                                           .dir = "backward") - round(0.029 * sampling_frequency)
         }
       }
       else{
@@ -319,7 +319,7 @@ server <- function(input, output, session){
                                            .dir = "backward") + 1
         }
         else{
-          jump_start_index <- jump_threshold_index - 29
+          jump_start_index <- jump_threshold_index - round(0.029 * sampling_frequency)
         }
       }
     }
